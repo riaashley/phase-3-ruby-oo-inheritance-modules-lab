db7aa43bf1142d0dd77263d14d2316a78e64cd6e
Ria Legaspi <riaveluz@gmail.com>
2023-02-18 21:24:36 -0800

    commit tree 8fecd46d23023bfd4fce921db4f65bdb9fdfca1b

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,10 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
   @@artists = []
 
   def initialize
@@ -19,13 +23,13 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def add_song(song)
     @songs << song
@@ -36,7 +40,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
